cba563ee73e3fd93d9ab07abb5bf2d275ed79aa4
KooTaehyeon <vcro12123@naver.com>
2023-05-03 20:54:26 +0900

    dummyData

--- src/model/dummyData.js
+++ src/model/dummyData.js
@@ -1,39 +1,39 @@
-const dummyData =[
+const dummyData = [
   {
     id: 1,
     price: 130000,
-    title: "[리코일] Recoil made in USA",
-    description: "리코일은 정말 좋은 상태관리 라이브러리에요",
+    title: '[리코일] Recoil made in USA',
+    description: '리코일은 정말 좋은 상태관리 라이브러리에요',
   },
   {
     id: 2,
     price: 90000,
-    title: "[리액트] React is good",
-    description: "리액트가 솔직히 짱이죠",
+    title: '[리액트] React is good',
+    description: '리액트가 솔직히 짱이죠',
   },
   {
     id: 3,
     price: 1000,
-    title: "[잡캐] 이것저것 좋아하는 잡캐",
-    description: "이것저것 많이 해보는게 틀린건 아니잖아요?",
+    title: '[공부] 상태관리',
+    description: '상태관리를 공부 해봅시다!!',
   },
   {
     id: 4,
     price: 1390000,
-    title: "[헨리] Henry made in Korea",
-    description: "여러분의 개발자라이프 응원할게요!",
+    title: '[태현] 태현 made in Korea',
+    description: '여러분의 개발자라이프 응원할게요!',
   },
   {
     id: 5,
     price: 90000,
-    title: "[리액트] React is good",
-    description: "리액트가 솔직히 짱이죠",
+    title: '[리액트] React is good',
+    description: '리액트가 솔직히 짱이죠',
   },
   {
     id: 6,
     price: 1390000,
-    title: "[헨리] Henry made in Korea",
-    description: "여러분의 개발자라이프 응원할게요!",
+    title: '[넥스트] Next is good',
+    description: '넥스트 공부도 하고있죠',
   },
-]
-export default dummyData
+];
+export default dummyData;
